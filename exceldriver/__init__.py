e41b3db4a9d2966e31b58010db416d5b2f8c724f
Nick DeRobertis <whoopnip@gmail.com>
2019-09-17 18:32:22 -0400

    Add docstring for main package

--- exceldriver/__init__.py
+++ exceldriver/__init__.py
@@ -1,1 +1,5 @@
+"""
+A tool used to work with Excel from Python. It currently mainly handles starting and stopping
+Excel, and getting the active Excel instance and workbook so that COM commands can be run on them.
+"""
 from .excel import Excel
